6ff614ca05f7bde4b71ab922db25ce324ccf15bb
hubbabubb <gergohuber@gmail.com>
2022-02-27 15:48:37 +0100

    add package-lock.json

---
+++ src/app/shared/service/footer.service.ts
@@ -0,0 +1,37 @@
+import {EventEmitter, Injectable, OnDestroy} from '@angular/core';
+import {BehaviorSubject, Subscription} from "rxjs";
+import {FooterProperties} from "../constants/footer.constants";
+import {Note} from "../../note/constants/note.constants";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FooterService implements OnDestroy {
+
+  private propsSubscription: Subscription;
+
+  saveEmitter = new EventEmitter<any>();
+  deleteEmitter = new EventEmitter<any>();
+
+  footerProperties: FooterProperties;
+  footerProperties$ = new BehaviorSubject<FooterProperties>({
+    editMode: false,
+    isAnyNote: false,
+    isAnyTodo: false,
+    trashOn: false,
+  });
+
+  constructor() {
+    this.subscribeForProperties();
+  }
+
+  private subscribeForProperties() {
+    this.propsSubscription = this.footerProperties$.subscribe(props => {
+      this.footerProperties = props;
+    })
+  }
+
+  ngOnDestroy() {
+    this.propsSubscription.unsubscribe();
+  }
+}
